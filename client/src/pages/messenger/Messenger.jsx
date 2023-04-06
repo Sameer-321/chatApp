ab282b3e7d4a3645c5dc8c4fa2873081847a1361
Sameer-321 <snrsameer1@gmail.com>
2023-04-06 22:33:03 +0545

    initiate Socket.io

--- client/src/pages/messenger/Messenger.jsx
+++ client/src/pages/messenger/Messenger.jsx
@@ -6,14 +6,35 @@ import ChatOnline from "../../components/chatOnline/ChatOnline";
 import { AuthContext } from "../../context/AuthContext";
 import { useEffect, useState, useContext, useRef } from "react";
 import axios from "axios";
+import { io } from "socket.io-client";
 
 export function Messenger() {
   const [conversations, setConversations] = useState([]);
   const [currentChat, setCurrentChat] = useState(null);
   const [messages, setMessages] = useState([]);
+  const socket = useRef()
   const [newMessages, setNewMessages] = useState("");
+
   const { user } = useContext(AuthContext);
   const scrollRef = useRef();
+
+  useEffect(()=>{
+    socket.current=io("ws://localhost:8900")
+  },[])
+
+  useEffect(() => {
+   socket.current.emit("addUser",user._id)
+   socket.current.on("getUsers",users=>{
+    console.log(users)
+   })
+  }, [user]);
+
+  useEffect(() => {
+    socket?.on("welcome", (message) => {
+      console.log(message);
+    });
+  }, [socket]);
+
   useEffect(() => {
     const getConversation = async () => {
       try {
@@ -27,7 +48,7 @@ export function Messenger() {
   }, [user._id]);
 
   useEffect(() => {
-    console.log(messages);
+    // console.log(messages);
     const getMessages = async () => {
       try {
         const res = await axios.get("/messages/" + currentChat?._id);
@@ -39,9 +60,9 @@ export function Messenger() {
     getMessages();
   }, [currentChat]);
 
-  useEffect(()=>{
-    scrollRef.current?.scrollIntoView({behaviour:"smooth"})
-  },[messages])
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behaviour: "smooth" });
+  }, [messages]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
